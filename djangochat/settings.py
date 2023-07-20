4765f44fe6e29169861d32a9a4a7d79c11e09855
Mayank <mayankawasthi258@gmail.com>
2023-07-20 10:23:29 +0530

    prod

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-#4wuqvqn^+-44@pd(hsg=*4zd3&337zb6ky5c+291^k*q73nij"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
