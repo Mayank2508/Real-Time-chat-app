85ed6ab392d6f9acd00f41c574c9c0f0cd121d80
Mayank <mayankawasthi258@gmail.com>
2023-08-20 21:36:39 +0530

    soumil fix

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -75,10 +75,23 @@ WSGI_APPLICATION = "djangochat.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = { 
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'neondb',
+    'USER': 'mayankawasthi258',
+    'PASSWORD': 'H4P1QjEwhZKS',
+    'HOST': 'ep-morning-cell-32188870.ap-southeast-1.aws.neon.tech',
+    'PORT': '5432',
+  }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
